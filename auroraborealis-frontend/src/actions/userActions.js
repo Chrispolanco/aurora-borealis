c916dbf519b5b15a5ff2a7762d9e81c20dc83350
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-01 14:34:34 -0700

    nested user in userActions

--- auroraborealis-frontend/src/actions/userActions.js
+++ auroraborealis-frontend/src/actions/userActions.js
@@ -7,7 +7,7 @@ export const loginUser = (user) => {
                 "Content-Type" : "application/json"
             }, 
             credentials: 'include',
-            body: JSON.stringify(user)
+            body: JSON.stringify({user: user})
         })
         .then(res => res.json())    
         .then(data => dispatch({ type: "LOGIN_USER", user: data }))
