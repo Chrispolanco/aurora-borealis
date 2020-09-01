111bd3953a2665741c2d4579dd5b97c7c2aba4f0
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-01 13:19:41 -0700

    modified user model with secure password

--- auroraborealis-frontend/src/reducers/userReducer.js
+++ auroraborealis-frontend/src/reducers/userReducer.js
@@ -1,7 +1,10 @@
-export const userReducer = (state={user: {}, loggedIn: false}, action ) => {
+export const userReducer = (state={user: {}, loggedIn: false, loading:false}, action ) => {
     switch(action.type) {
         case 'LOGIN_USER': 
-            return {user: action.user, loggedIn: true}
+            return {user: action.user, loggedIn: true, loading: false}
+
+        case 'LOADING': 
+            return {...state, loading: true}
 
         default: 
             return state; 
