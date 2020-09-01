d3fc52e3be7a235501b0444e66d63492c8f8c940
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-01 00:44:34 -0700

    added login basics to frontend

---
+++ auroraborealis-frontend/src/components/Login.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'; 
+
+class Login extends Component {
+    state = {
+        email: "", 
+        password: ""
+    }
+
+    handleOnChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value 
+        })
+    }
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+
+        this.setState({
+            email: "", 
+            password: ""
+        })
+    }
+
+    render() {
+        return(
+            <div> 
+                <form onSubmit= {this.handleOnSubmit}> 
+                    <input type="email" name="email" value={this.state.email} onChange={handleOnChange} />
+                    <input type="text" name="password" value={this.state.password} onChange={this.handleOnChange} /> 
+                    <input type="submit" /> 
+                </form>
+            </div> 
+        )
+    }
+}
+
+export default Login 
