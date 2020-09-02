cca0983cdb1fce77354490b001cbeb0492535fcf
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-01 23:32:16 -0700

    fixing token so password stays hidden

--- auroraborealis-backend/app/controllers/sessions_controller.rb
+++ auroraborealis-backend/app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
 
     def create
-        @user = User.find_by(:email => params[:user][:email])
-        binding.pry
-        if @user && @user.authenticate(params[:user][:password])
-            token = encode_token(@user)
-            render json: {user: @user, token: token}
-        else 
-            render json: {
-                error: "User not authenticated"
+        user = User.find_by(:email => params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            token = encode_token(id: user.id)
+            userObj = {
+                id: user.id, 
+                username: user.username, 
+                email: user.email, 
+                first_name: user.first_name, 
+                last_name: user.last_name, 
+                token: token
             }
+            render json: {userObj: user}
+        else 
+            render json: { error: "User not authenticated" }
         end 
     end
 
