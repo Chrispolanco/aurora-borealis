54acdc5b416a694ba3f62ac3c23fa4203e40c03b
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-02 14:29:32 -0700

    updated models

--- auroraborealis-backend/app/models/user.rb
+++ auroraborealis-backend/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, :password, presence: true 
 
     has_many :posts 
-    has_many 
+    has_many :comments, through: :posts
 end
