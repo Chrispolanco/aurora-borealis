8395c9e467ef4468f7b3c90c7a5de4519287eeab
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-03 14:02:29 -0700

    removed jwt steps used old method

--- auroraborealis-backend/app/controllers/application_controller.rb
+++ auroraborealis-backend/app/controllers/application_controller.rb
@@ -2,19 +2,20 @@ class ApplicationController < ActionController::API
     include ::ActionController::Cookies
 
     def current_user
-        User.find_by(decode_token: params[:id])
+        User.find_by(id: session[:user_id])
     end 
 
     def logged_in? 
         !!current_user
     end 
 
-    def encode_token(payload)
-        JWT.encode(payload, "secret")   
-    end 
+    # def encode_token(payload)
+    #     JWT.encode(payload, "secret")   
+    # end 
 
-    def decode_token 
-        jwt = cookies.signed[:jwt]
-        JWT.decode(jwt,"secret")
-    end 
+    # def authenicate_user
+    #     jwt = cookies.signed[:jwt]
+    #     JWT.decode(jwt,"secret")
+    # end 
+
 end
--- auroraborealis-backend/app/controllers/sessions_controller.rb
+++ auroraborealis-backend/app/controllers/sessions_controller.rb
@@ -1,28 +1,61 @@
 class SessionsController < ApplicationController
 
-    def new 
-        user = User.new
-        render json: UserSerializer.new(user).to_serialized_json
+    def create 
+        user = User.find_by(username: [:session][:username])
+
+        if user & user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            render json: UserSerializer.new(user), status: 200 
+        else
+            render json: {
+                error: "Incorrect username/password"
+            }
+        end 
     end 
 
-    def create
-        user = User.find_by(:email => params[:user][:email])
+    def destory
+        session.clear
+        render json: {
+            notice: "Successfully logged out"
+        }, 
+            status: 200
+    end 
 
-        if user && user.authenticate(params[:user][:password])
-            token = encode_token(id: user.id)
-            cookies.signed[:jwt] = {value: token, httponly: true}
-
-            userObj = {
-                username: user.username, 
-                email: user.email, 
-                first_name: user.first_name, 
-                last_name: user.last_name, 
-                token: token
-            }
-            render json: userObj, status: 200
+    def get_current_user
+        if logged_in 
+            render json: UserSerializer.new(current_user).to_serialized_json 
         else 
-            render json: { error: "User not authenticated" }, status: 404
+            render json: {
+                error: "Log in required"
+            }
         end 
-    end
+    end 
 
+
+
+
+    # def create
+    #     user = User.find_by(:email => params[:user][:email])
+
+    #     if user && user.authenticate(params[:user][:password])
+    #         token = encode_token(id: user.id)
+    #         cookies.signed[:jwt] = {value: token, httponly: true, expires: 1.hour.from_now}
+
+    #         userObj = {
+    #             username: user.username, 
+    #             email: user.email, 
+    #             first_name: user.first_name, 
+    #             last_name: user.last_name, 
+    #             token: token
+    #         }
+    #         render json: userObj, status: 200
+    #     else 
+    #         render json: { error: "User not authenticated" }, status: 404
+    #     end 
+    # end
+
+    # def destory 
+    #     cookies.delete(:jwt)
+    #     render json:{}
+    # end 
 end 
