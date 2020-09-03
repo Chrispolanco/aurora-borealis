8fd51e47553d8b3ab70c4ab9a762a86393778ea8
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-03 00:38:24 -0700

    created custom user serializer

--- auroraborealis-backend/app/serializers/user_serializer.rb
+++ auroraborealis-backend/app/serializers/user_serializer.rb
@@ -1,3 +1,20 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email 
+
+  def initialize(user_object)
+    @user = user_object
+  end 
+
+  def to_serialized_json
+    options = {
+      include: {
+        posts: {
+          only: [:id, :image, :description, :votes, :latitude, :longitude, :date, :created_at, :user_id]
+        }
+      },
+      only: [:id, :username, :email, :first_name, :last_name]
+    }
+    @user.to_json(options)
+  end 
+
 end
