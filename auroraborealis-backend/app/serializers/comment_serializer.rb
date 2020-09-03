835c28ed99ef67b0869ad72242e841d11a66eef4
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-03 00:30:14 -0700

    created custom serializer for comment

--- auroraborealis-backend/app/serializers/comment_serializer.rb
+++ auroraborealis-backend/app/serializers/comment_serializer.rb
@@ -1,3 +1,14 @@
-class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :context
-end
+class CommentSerializer
+
+    def initialize(comment_object)
+      @comment = comment_object
+    end 
+  
+    def to_serialized_json
+      options = {
+        only: [:id, :context, :post_id, :user_id]
+      }
+      @comment.to_json(options)
+    end 
+  
+  end
