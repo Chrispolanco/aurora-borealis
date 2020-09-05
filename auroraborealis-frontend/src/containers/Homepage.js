b2d6e1607d41217bed9e9cbcd8dff54f65493ca0
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-05 00:42:37 -0700

    added homepage framework

---
+++ auroraborealis-frontend/src/containers/Homepage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'; 
+import Login from './components/Login'; 
+import Signup from '.components/Signup'; 
+import { connect } from 'react-redux';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div> 
+                <Signup addUser={this.props.addUser} /> 
+            </div>
+        )
+    }
+}
+
+    const mapDispatchToProps = (dispatch) => ({
+        addUser: (user) => dispatch({ type: 'ADD_USER', user})
+    })
+
+export default connect(null, mapDispatchToProps)(Homepage); 
