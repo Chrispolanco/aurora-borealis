e851ca6c62e8266508499a3666cd910f3f4d507c
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-06 22:13:40 -0700

    added test sign up

--- auroraborealis-frontend/src/actions/currentUser.js
+++ auroraborealis-frontend/src/actions/currentUser.js
@@ -13,6 +13,22 @@ export const blankCurrentUser = () => {
 
 export const signup = (credentials) => {
     return(dispatch) => {
-        return fetch()
+        return fetch('http://localhost:3001/login',{
+            credentials: "include", 
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify(credentials)
+        })
+        .then(response => {return response.json()})
+        .then(user => {
+            if(user.error){
+                alert(user.error)
+            } else{
+                dispatch({ type: "CURRENT_USER", user: user})
+            }
+        })
+        .catch(console.log)
     }
 }
