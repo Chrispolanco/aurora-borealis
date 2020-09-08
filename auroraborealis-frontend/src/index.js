8cbafd4b04d53fb0d660b507f6f543e0e019a654
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-08 01:25:44 -0700

    connected currentUserReducer

--- auroraborealis-frontend/src/index.js
+++ auroraborealis-frontend/src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'; 
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'; 
 import thunk from 'redux-thunk'; 
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom'; 
-import testReducer from './reducers/testReducer'; 
+import testReducer from './reducers/testReducer'
+import currentUserReducer from './reducers/currentUserReducer'
 
-// const reducer = combineReducers({
-//   testReducer
-// })
+const reducer = combineReducers({
+  testReducer, 
+  currentUserReducer
+})
 
-const store = createStore(testReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 
 ReactDOM.render(
