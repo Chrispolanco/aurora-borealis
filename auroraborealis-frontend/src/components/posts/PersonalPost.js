1692e8224e66acf78204468e670da01552a48b43
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-14 21:13:44 -0700

    added comment action and commentform framework

--- auroraborealis-frontend/src/components/posts/PersonalPost.js
+++ auroraborealis-frontend/src/components/posts/PersonalPost.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'; 
+import CommentCard from '../comments/commentCard'
 
 const PersonalPost = ({ post }) => {
+    const date = new Date(post.created_at).toLocaleDateString()
     return(
         <div className="Personal_Post" key={post.id} > 
-            <h1> {post.user_id} </h1>
+            <p>Date Submitted:{date}</p>
+            <p>User ID:{post.user_id} </p>
+            <p>Description:{post.description}</p>
+            <p>Comments:</p>
+            {post.comments.map(comment =>
+                <CommentCard comment={comment} key={comment.id}/>)}
+            <p>Add Comment:</p>
         </div>
     )
 }
