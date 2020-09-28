f02cd2375f005aa94beba3bd7f2f7cdb45b8d53e
Chrispolanco <chrispolanco@sbcglobal.net>
2020-09-27 21:24:26 -0700

    comments now sorted newest to oldest

--- auroraborealis-frontend/src/components/posts/PersonalPost.js
+++ auroraborealis-frontend/src/components/posts/PersonalPost.js
@@ -4,6 +4,11 @@ import CommentForm from '../comments/CommentForm'
 
 const PersonalPost = ({ post, userLoggedIn }) => {
     const date = new Date(post.created_at).toLocaleDateString()
+    const oneComment = post.comments.filter(comment => 
+        comment.context.length > 0)
+    const sortedComments = oneComment.map(comment => {
+    return <CommentCard comment={comment} key={comment.id}/>})
+
     return(
         <div className="PersonalPostContainer" key={post.id} > 
             {/* <p>Votes: {post.votes}</p> */}
@@ -12,11 +17,13 @@ const PersonalPost = ({ post, userLoggedIn }) => {
             <h4>Story</h4>
             <div className="story">{post.story}</div>
             <p>Comments:</p>
-                {post.comments.map(comment =>
-                <CommentCard comment={comment} key={comment.id}/>)}
+            {sortedComments.sort((a, b) => (a.created_at > b.created_at) ? 1 : -1)}
             <CommentForm post={post} userLoggedIn={userLoggedIn} /> 
         </div>
     )
 }
 
 export default PersonalPost
+
+// {post.comments.map(comment =>
+//     <CommentCard comment={comment} key={comment.id}/>)}
