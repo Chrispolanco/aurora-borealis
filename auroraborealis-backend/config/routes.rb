385494cb9aff70c7178ddd2c661797f454a94efe
Chrispolanco <chrispolanco@sbcglobal.net>
2020-11-19 23:51:53 -0800

    added root to config

--- auroraborealis-backend/config/routes.rb
+++ auroraborealis-backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'sessions#create'
+
   post "/login", to: "sessions#create"
   post "/signup", to: "users#create"
   delete "/logout", to: "sessions#destroy"
