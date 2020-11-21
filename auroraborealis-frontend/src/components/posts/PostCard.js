54154dc2bd9892ded1bfacbe1003a7d622a29871
Chrispolanco <chrispolanco@sbcglobal.net>
2020-11-21 01:15:19 -0800

    commented out more code

--- auroraborealis-frontend/src/components/posts/PostCard.js
+++ auroraborealis-frontend/src/components/posts/PostCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import CommentCard from '../comments/CommentCard'; 
-import CommentForm from '../comments/CommentForm'
+// import CommentCard from '../comments/CommentCard'; 
+// import CommentForm from '../comments/CommentForm'
 
 const PostCard = ({ post, userLoggedIn }) => {
     const date = new Date(post.created_at).toLocaleDateString()
