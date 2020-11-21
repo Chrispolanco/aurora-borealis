f6c21c194b0ce8abe67bb5e2cb647bdde89a23a3
Chrispolanco <chrispolanco@sbcglobal.net>
2020-11-21 00:11:37 -0800

    Updates

---
+++ precache-manifest.59f1d8fc6fafcc5aa17eaffa3c5a52a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46521c98bf1577c0ebc06ded9c76defd",
+    "url": "/auroraborealis-frontend/index.html"
+  },
+  {
+    "revision": "4ebd729a2577a93c87fe",
+    "url": "/auroraborealis-frontend/static/css/main.d9adc3e0.chunk.css"
+  },
+  {
+    "revision": "07c5a06b69b7ff8968d8",
+    "url": "/auroraborealis-frontend/static/js/2.be18ce0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/auroraborealis-frontend/static/js/2.be18ce0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebd729a2577a93c87fe",
+    "url": "/auroraborealis-frontend/static/js/main.1467857f.chunk.js"
+  },
+  {
+    "revision": "4b33d3815119567dc317",
+    "url": "/auroraborealis-frontend/static/js/runtime-main.2998e010.js"
+  },
+  {
+    "revision": "e3be8d243e31ba8a9708fcf8ec2a0350",
+    "url": "/auroraborealis-frontend/static/media/1.e3be8d24.png"
+  }
+]);
