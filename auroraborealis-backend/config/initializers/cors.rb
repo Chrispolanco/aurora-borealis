6c2b631733a6d2393f6f9ed2ad403bd2d5edcff1
Chrispolanco <chrispolanco@sbcglobal.net>
2020-11-21 22:57:09 -0800

    updated origins in cors backend

--- auroraborealis-backend/config/initializers/cors.rb
+++ auroraborealis-backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'https://wizardly-einstein-678d9e.netlify.app/'
 
     resource '*',
     headers: :any,
